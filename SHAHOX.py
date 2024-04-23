9efc9589487aadd627b12179ad7038edcbce1cff
shaharia420 <134796377+shaharia420@users.noreply.github.com>
2024-04-23 14:48:52 +0600

    Update SHAHOX.py

--- SHAHOX.py
+++ SHAHOX.py
@@ -3,7 +3,7 @@
 import os,sys,time,json,random,re,string,platform,base64,uuid
 from bs4 import BeautifulSoup as sop
 from bs4 import BeautifulSoup
-import requests as ress
+#import requests as ress
 from datetime import date
 from datetime import datetime
 from time import sleep
